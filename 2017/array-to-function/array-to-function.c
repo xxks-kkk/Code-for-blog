b7af8d444032aa86d68612afe7bfa99a734fe493
Zeyuan On Hadoop <ferrishu3886@gmail.com>
2017-01-08 16:31:37 +0800

    Finish array-to-function.c

--- 2017/array-to-function/array-to-function.c
+++ 2017/array-to-function/array-to-function.c
@@ -6,14 +6,17 @@
 void print(int* array, int length);
 void change(int *array,int length);
 void change2(int *array, int length);
+void change3(int **array, int length);
 
 void test_change();
 void test_change2();
+void test_change3();
 
 int main(){
 
   test_change();  printf("\n");
   test_change2(); printf("\n");
+  test_change3(); printf("\n");
   
   return 0;
 }
@@ -42,6 +45,19 @@ void change2(int *array,int length)
   array = tmp;
 }
 
+void
+change3(int **array, int length)
+{
+  int* tmp = calloc(length, sizeof(int));
+  int i;
+  for (i = 0; i < length; i++)
+  {
+    *(tmp+i) = 5;
+  }
+  free(*array);
+  *array = tmp;
+}
+
 void test_change()
 {
   printf("TEST: change\n");
@@ -60,13 +76,30 @@ void test_change()
 void test_change2()
 {
   printf("TEST: change2\n");
-  int i, length = 3;
+  int length = 3;
   int test[3] = {1,2,3};
 
   printf("Before:");
   print(test, length);
   printf("before change, test address: %p\n", test);
-  change2(test, 3);
+  change2(test, length);
+  printf("After:");
+  print(test, length);
+  printf("after change, test address: %p\n", test);
+}
+
+void test_change3()
+{
+  printf("TEST: change3\n");
+  int i, length = 3;
+  int* test = calloc(length, sizeof(int));
+  test[0] = 1;
+  test[1] = 2;
+  test[2] = 3;
+  printf("Before:");
+  print(test, length);
+  printf("before change, test address: %p\n", test);
+  change3(&test, length);
   printf("After:");
   print(test, length);
   printf("after change, test address: %p\n", test);
