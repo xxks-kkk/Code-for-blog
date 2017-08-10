93b941130011062f1066c3b5cdcba8e4ec5ad89d
Zeyuan Hu <ferrishu3886@gmail.com>
2017-08-10 05:37:58 -0500

    inital drop

---
+++ 2017/knapsack/knapsack.c
@@ -0,0 +1,230 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 
+ * [Problem]:
+ * In our Materials Marketplace we have many different companies all looking 
+ * for the same material. However, each company is looking for a specific 
+ * quantity of the material at a price they set themselves. 
+ * A company approaches our team with a large amount of that material, 
+ * but not enough to complete every request for it. 
+ * Given the total amount of the material they have, 
+ * the company asks us to find out what companies they should sell to in order 
+ * to maximize their profits. This scenario happens frequently so we need to 
+ * be able to compute the answer relatively quickly and with minimal processing power.
+ * 
+ * Company| A  | B  | C  | D  | E  | F  | G  | H  | I  | J  |
+ * -------+----+----+----+----+----+----+----+----+----+----+
+ * Amount | 1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  | 9  | 10 |
+ * -------+----+----+----+----+----+----+----+----+----+----+
+ * Price  | 1  | 5  | 8  | 9  | 10 | 17 | 17 | 20 | 24 | 30 | 
+ * -------+----+----+----+----+----+----+----+----+----+----+
+ *
+ * [Reference]: 
+ * 1. http://cse.unl.edu/~goddard/Courses/CSCE310J/Lectures/Lecture8-DynamicProgramming.pdf
+ *
+ */
+
+/* Utility function */
+void printList(int*, int);
+
+/* Test cases */
+void testCase1();
+void testCase2();
+
+/* The problem can be treated as 0/1 knapsack problem. */
+int* knapsack(int maxWeight, int numItems, int** elements, int* resultLength);
+
+int
+main(void)
+{
+  testCase1();
+  testCase2();
+  return 0;
+}
+
+/*
+ * The procedure solves the problem above.
+ *
+ * [Return]:
+ *  - The procedure returns an array of companies that maximizes
+ *    our profit
+ *
+ * [Parameters]:
+ *  - maxWeight : the amount of material we have
+ *  - numItems  : the number of companies we receive the requests from
+ *  - elements  : 2D array that have two rows: first row contains the
+ *                amount of material that each company needs and second
+ *                row contains the corresponding price that each company wants
+ *                to pay
+ *  - resultLength : number of companies we choose to maximize our profit
+ */
+int*
+knapsack(int maxWeight, int numItems, int** elements, int* resultLength)
+{
+  int** table = malloc(sizeof(int*) * (numItems+1));
+  int w, i, w_i, b_i, k;
+  for(i = 0; i <= numItems; i++)
+  {
+    table[i] = malloc(sizeof(int) * (maxWeight+1));
+  }
+  for(w = 0; w <= maxWeight; w++)
+    table[0][w] = 0;
+  for(i = 1; i <= numItems; i++)
+    table[i][0] = 0;
+  for(i = 1; i <= numItems; i++)
+  {
+    for(w = 1; w <= maxWeight; w++)
+    {
+      w_i = elements[0][i-1];
+      b_i = elements[1][i-1];
+      if(w_i <= w)
+      {
+        if(b_i + table[i-1][w-w_i] > table[i-1][w])
+          table[i][w] = b_i + table[i-1][w-w_i];
+        else
+          table[i][w] = table[i-1][w];
+      }
+      else
+      {
+        table[i][w] = table[i-1][w];
+      }
+    }
+  }
+  // max profit: table[numItems][maxWeight];
+  // now we need to find out exactly composition of the knapsack items
+  int *result = malloc(sizeof(int) * numItems);
+  i = numItems;
+  k = maxWeight;
+  int counter = 0; 
+  while (i > 0 && k > 0)
+  {
+    if(table[i][k] != table[i-1][k])
+    {
+      result[counter] = i; // we mark the ith item as in the knapsack
+      counter++;
+      i--;
+      k -= elements[0][i-1];
+    }
+    else
+    {
+      i--;
+    }
+  }
+  *resultLength = counter;
+  for(i=0; i <= numItems; i++)
+  {
+    free(table[i]);
+  }
+  free(table);
+  return result;
+}
+
+void
+printList(int* result, int resultLength)
+{
+  int i;
+  printf("Company we choose: ");
+  for (i = 0; i < resultLength; i++)
+  {
+    printf("%c, %s", result[i]+65, (i == resultLength-1) ? ("\n") : (""));
+  }
+  printf("\n");
+}
+
+/*
+ * Company| A  | B  | C  | D  |
+ * -------+----+----+----+----+
+ * Amount | 2  | 3  | 4  | 5  |
+ * -------+----+----+----+----+
+ * Price  | 3  | 4  | 5  | 6  |
+ * -------+----+----+----+----+
+ */
+void
+testCase1()
+{
+  printf("%s\n", "TEST CASE 1");
+
+  // Create test data
+  int maxWeight, numItems;
+  maxWeight = 5;
+  numItems = 4;
+  int** elements = malloc(sizeof(int*) * 2);
+  int i, j;
+  for(i = 0 ; i < 2; i++)
+  {
+    elements[i] = malloc(sizeof(int) * numItems);
+  }
+  elements[0][0] = 2;
+  elements[0][1] = 3;
+  elements[0][2] = 4;
+  elements[0][3] = 5;
+  elements[1][0] = 3;
+  elements[1][1] = 4;
+  elements[1][2] = 5;
+  elements[1][3] = 6;
+
+  // Get the solution
+  int resultLength;
+  int* result = knapsack(maxWeight,numItems,elements, &resultLength);
+
+  // Printout the solution
+  printList(result, resultLength);
+  
+  // Cleanup
+  free(result);
+}
+
+/*
+ * Company| A  | B  | C  | D  | E  | F  | G  | H  | I  | J  |
+ * -------+----+----+----+----+----+----+----+----+----+----+
+ * Amount | 1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  | 9  | 10 |
+ * -------+----+----+----+----+----+----+----+----+----+----+
+ * Price  | 1  | 5  | 8  | 9  | 10 | 17 | 17 | 20 | 24 | 30 | 
+ * -------+----+----+----+----+----+----+----+----+----+----+
+ */
+void
+testCase2()
+{
+  printf("%s\n", "TEST CASE 2");
+  // Create test data
+  int maxWeight, numItems;
+  maxWeight = 30;
+  numItems = 10;
+  int** elements = malloc(sizeof(int) * 2);
+  int i, j;
+  for(i = 0 ; i < 2; i++)
+  {
+    elements[i] = malloc(sizeof(int) * numItems);
+  }
+  elements[0][0] = 1;
+  elements[0][1] = 2;
+  elements[0][2] = 3;
+  elements[0][3] = 4;
+  elements[0][4] = 5;
+  elements[0][5] = 6;
+  elements[0][6] = 7;
+  elements[0][7] = 8;
+  elements[0][8] = 9;
+  elements[0][9] = 10;
+  elements[1][0] = 1;
+  elements[1][1] = 5;
+  elements[1][2] = 8;
+  elements[1][3] = 9;
+  elements[1][4] = 10;
+  elements[1][5] = 17;
+  elements[1][6] = 17;
+  elements[1][7] = 20;
+  elements[1][8] = 24;
+  elements[1][9] = 30;
+
+  // Get the solution
+  int resultLength;
+  int* result = knapsack(maxWeight,numItems,elements, &resultLength);
+
+  // Printout the solution
+  printList(result, resultLength);
+  
+  // Cleanup
+  free(result);
+}
