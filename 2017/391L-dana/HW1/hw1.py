85305ca9a75b2a5fdf90e6d5f0b7149e56435cf3
Zeyuan Hu <ferrishu3886@gmail.com>
2017-09-14 14:34:19 -0500

    Add hw1.py

--- 2017/391L-dana/HW1/hw1.py
+++ 2017/391L-dana/HW1/hw1.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 
+## HW1 | Principle Component Analysis (PCA) and K-Nearest Neighbour
+## Clustering
+##
+## AUTHOR: Zeyuan Hu <iamzeyuanhu@utexas.edu | EID: zh4378>
+##
+## INSTRUCTIONS
+## ------------
+##
+##
+
 import numpy as np
 from scipy import linalg as la
 import scipy.io as spio
+import scipy.stats as spstats
 import matplotlib.pyplot as plt
+from sklearn import neighbors
 
 ## Read in the data file
 mat = spio.loadmat('digits.mat')
@@ -13,9 +25,9 @@ trainImages = mat['trainImages']
 trainLabels = mat['trainLabels']
 
 ## Number of training images we use
-M = 4000
+M = 1000
 
-## Set nRedDim (i.e., k)
+## Set nRedDim
 nRedDim = 1
 
 ## Construct our data matrix with dimension 784xM. 784 is calculated by
@@ -40,7 +52,7 @@ indices = indices[::-1]     # change to descending order
 evecs = evecs[:,indices]
 evals = evals[indices]
 
-## Try to determine the number of principal component K
+## Try to determine the number of principal component nRedDim
 U,S,V = np.linalg.svd(C)
 print('X  Rate')
 for x in range(1,M):
@@ -50,17 +62,43 @@ for x in range(1,M):
         nRedDim = x
         break
 
-## select the top K eigenvectors
+## select the top nRedDim eigenvectors
 evecs = evecs[:,:nRedDim]
 
 ## normalize eigenvectors
 evecs = np.divide(evecs, np.linalg.norm(evecs, axis=0))
 
-## Produce the new data matrix
 x = np.dot(np.transpose(evecs), data)
-
 ## Compute the original data
 y = np.dot(evecs,x)+np.tile(mu.reshape(784,1),M)
 
 plt.imshow(y[:,1].reshape(28,28).real) 
 plt.show()
+
+# Project the test data to the eigenspace
+# n_neighbors = 3
+# #num_testImages = testImages.shape[3]
+# num_testImages = 300
+# testImages_use = testImages[:,:,0,0:num_testImages].reshape(784, num_testImages)
+# test_mu = np.mean(testImages_use,axis=1)
+# testImages_normalize = testImages_use -  np.tile(test_mu.reshape(784,1),num_testImages).astype(testImages_use.dtype)
+# testImages_es = np.dot(evecs.T,testImages_normalize)
+# trainImages_es = np.dot(np.transpose(evecs), data)
+# trainLabels_arr = np.squeeze(trainLabels)
+# testLabels_arr = testLabels.squeeze()
+# num_trainImages = data.shape[1]
+# classification_result = np.zeros(testLabels.shape[1])
+
+# for x in range(num_testImages):
+#     distance = np.zeros(num_trainImages)
+#     for y in range(num_trainImages):
+#         distance[y] = np.linalg.norm(testImages_es[:,x] - trainImages_es[:,y])
+#     classification_result[x] = spstats.mode(trainLabels_arr[distance.argsort()[-n_neighbors:]])[0].item(0)
+#     print(classification_result[x])
+
+# accuracy = np.sum(classification_result == testLabels_arr) / float(testLabels.shape[1])
+# print(accuracy)
+
+# # M = 400 11.35%
+# # M = 500 11.35%
+# # M = 600 11.35%
