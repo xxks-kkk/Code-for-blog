5d36a1026a8390278b678038e185973437abb64f
Zeyuan Hu <ferrishu3886@gmail.com>
2018-02-16 01:11:06 -0600

    Finish part 1

--- 2018/439-alison/p0/handle.c
+++ 2018/439-alison/p0/handle.c
@@ -17,9 +17,41 @@
  * Finally, loop forever, printing "Still here\n" once every
  * second.
  */
+
+void handler2(int sig)
+{
+    if (sig == SIGINT)
+    {
+        ssize_t bytes;
+        const int STDOUT = 1;
+        char *msg = "Nice try.\n";
+        bytes = write(STDOUT, msg, sizeof(msg));
+    }
+    else if (sig == SIGUSR1)
+    {
+        ssize_t bytes;
+        const int STDOUT = 1;
+        char *msg = "exiting";
+        bytes = write(STDOUT, msg, sizeof(msg));
+        exit(1);
+    }
+}
+
 int main(int argc, char **argv)
 {
-  return 0;
+    pid_t pid;
+    pid = getpid();
+    printf("Process ID: %d\n", pid);
+    struct timespec ts;
+    ts.tv_sec = 5;
+    Signal(SIGINT, handler2);
+    Signal(SIGUSR1, handler2);
+    while(1)
+    {
+        if (nanosleep(&ts, NULL) == 0)
+            // nanosleep returns 0 if successfully sleeping for the requested interval
+            printf("Still here\n");
+    }
 }
 
 
--- 2018/439-alison/p0/mykill.c
+++ 2018/439-alison/p0/mykill.c
@@ -5,9 +5,26 @@
 #include <signal.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
 
+/*
+ * unix_error - unix-style error routine.
+ */
+inline static void unix_error(char *msg)
+{
+    fprintf(stdout, "%s: %s\n", msg, strerror(errno));
+    exit(1);
+}
 
 int main(int argc, char **argv)
 {
-  return 0;
+    if(argc != 2){
+        fprintf(stderr, "Usage: mykill <num>\n");
+        exit(-1);
+    }
+    int arg;
+    arg = atoi(argv[1]);
+    if (kill(arg, SIGUSR1) != 0)
+        unix_error("Process ID doesn't exist");
+    return 0;
 }
