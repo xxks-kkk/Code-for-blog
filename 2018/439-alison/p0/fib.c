f6c54c13f11d81d7f6348dd8542823ee3ade94a5
Zeyuan Hu <ferrishu3886@gmail.com>
2018-08-04 21:27:50 -0500

    save

--- 2018/439-alison/p0/fib.c
+++ 2018/439-alison/p0/fib.c
@@ -10,84 +10,134 @@
 
 const int MAX = 13;
 
-static void doFib(int n, int doPrint);
-static pid_t Fork(void);
+static void
+doFib( int n, int doPrint );
+static void
+doFibHelper( int n, int doPrint, int depth );
+static pid_t
+Fork( void );
+
 
 /*
  * unix_error - unix-style error routine.
  */
-inline static void unix_error(char *msg)
+inline static void
+unix_error( char *msg )
 {
-    fprintf(stdout, "%s: %s\n", msg, strerror(errno));
-    exit(1);
+    fprintf( stdout, "%s: %s\n", msg, strerror(errno));
+    exit( 1 );
 }
 
 
-int main(int argc, char **argv)
+int
+main( int argc, char **argv )
 {
     int arg;
-    int print=1;
+    int print = 2;
 
-    if(argc != 2){
-        fprintf(stderr, "Usage: fib <num>\n");
-        exit(-1);
+    if ( argc != 2 )
+    {
+        fprintf( stderr, "Usage: fib <num>\n" );
+        exit( -1 );
     }
 
-    arg = atoi(argv[1]);
-    if(arg < 0 || arg > MAX){
-        fprintf(stderr, "number must be between 0 and %d\n", MAX);
-        exit(-1);
+    arg = atoi( argv[1] );
+    if ( arg < 0 || arg > MAX )
+    {
+        fprintf( stderr, "number must be between 0 and %d\n", MAX );
+        exit( -1 );
     }
-
-    doFib(arg, print);
+    doFib( arg, print );
 
     return 0;
 }
 
-/* 
+
+/*
  * Recursively compute the specified number. If print is
  * true, print it. Otherwise, provide it to my parent process.
  *
  * NOTE: The solution must be recursive and it must fork
  * a new child for each call. Each process should call
  * doFib() exactly once.
+ *
+ * doPrint = 2 will print out the process numbering and its return value
+ * doPrint = 1 will only print out the final result
  */
-static void doFib(int n, int doPrint)
+static void
+doFib( int n, int doPrint )
 {
-  pid_t pid1, pid2;
-  int res = 0, status;
-  //Base case, return n if n<2
-  if (n <= 1){
-    exit(n);
-  }
-  pid1 = Fork();
-  /* Code executed by first child */
-  if (pid1 == 0) {
-    doFib(n-1, 0);
-  }
-  /* Code executed by second child */
-  pid2 = Fork();
-  if (pid2 == 0) {
-    doFib(n-2, 0);
-  }
-  //Reap first Child
-  while(waitpid(pid1, &status, 0) > 0)
-    if (WIFEXITED(status))
-      res += WEXITSTATUS(status);
-  //Reap second child
-  while(waitpid(pid2, &status, 0) > 0)
-    if (WIFEXITED(status))
-      res += WEXITSTATUS(status);
-  if(doPrint){
-    printf("%d\n", res);
-  }
-  exit(res);
+    int fd[2];
+    int depth = 0;
+    pipe( fd );
+    doFibHelper( n, doPrint, depth );
 }
 
-static pid_t Fork(void)
+
+void
+doFibHelper( int n, int doPrint, int depth )
 {
-  pid_t pid;
-  if ((pid = fork()) < 0)
-    unix_error("Fork error");
-  return pid;
+    pid_t pid1, pid2;
+    int res = 0, status;
+    //Base case, return n if n<2
+    if ( n <= 1 )
+    {
+        exit( n );
+    }
+    printf( "Child with id: %d and its Parent id: %d \n", getpid(), getppid());
+    pid1 = Fork();
+    /* Code executed by first child */
+    if ( pid1 == 0 )
+    {
+        printf( "Child with id: %d and its Parent id: %d \n", getpid(), getppid());
+        if ( doPrint == 2 )
+        {
+            doFibHelper( n - 1, doPrint, depth + 1 );
+        }
+        else
+        {
+            doFibHelper( n - 1, 0, depth + 1 );
+        }
+    }
+    /* Code executed by second child */
+    pid2 = Fork();
+    if ( pid2 == 0 )
+    {
+        printf( "Child with id: %d and its Parent id: %d \n", getpid(), getppid());
+        if ( doPrint == 2 )
+        {
+            doFibHelper( n - 2, doPrint, depth + 1 );
+        }
+        else
+        {
+            doFibHelper( n - 2, 0, depth + 1 );
+        }
+    }
+    //Reap first Child
+    while ( waitpid( pid1, &status, 0 ) > 0 )
+        if ( WIFEXITED( status ))
+            res += WEXITSTATUS( status );
+    //Reap second child
+    while ( waitpid( pid2, &status, 0 ) > 0 )
+        if ( WIFEXITED( status ))
+            res += WEXITSTATUS( status );
+    if ( depth == 0 )
+    {
+        printf( "process numbering: %d \t return val: %d\n", depth, res );
+    }
+    else if ( doPrint == 1 )
+    {
+        printf( "%d\n", res );
+    }
+    exit( res );
+}
+
+
+static pid_t
+Fork( void )
+{
+    pid_t pid;
+    if ((pid = fork()) < 0 )
+        unix_error( "Fork error" );
+    return pid;
 }
