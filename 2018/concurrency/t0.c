1812ac1e2c9a87cedec41ad0def4756f5a6fa177
Zeyuan Hu <ferrishu3886@gmail.com>
2018-10-07 00:04:33 -0500

    Add code

---
+++ 2018/concurrency/t0.c
@@ -0,0 +1,40 @@
+/*************************************************************************
+  > File Name:       t0.c
+  > Author:          Zeyuan Hu
+  > Mail:            ferrishu3886@gmail.com
+  > Created Time:    10/3/18
+  > Description:
+    
+    Demo of thread creation in C.
+ ************************************************************************/
+
+#include <stdio.h>
+#include <assert.h>
+#include <pthread.h>
+
+void *mythread(void *arg)
+{
+    printf("%s\n", (char *) arg);
+    return NULL;
+}
+
+int
+main(int argc, char *argv[])
+{
+    pthread_t p1, p2;
+    int rc;
+    printf("main: begin\n");
+    rc = pthread_create(&p1, NULL, mythread, "A");
+    assert(rc == 0);
+    rc = pthread_create(&p2, NULL, mythread, "B");
+    assert(rc == 0);
+    // join waits for the threads to finish
+    // The pthread_join() function suspends execution of the calling thread
+    // until the target thread terminates
+    rc = pthread_join(p1, NULL);
+    assert(rc == 0);
+    rc = pthread_join(p2, NULL);
+    assert(rc == 0);
+    printf("main: end\n");
+    return 0;
+}
