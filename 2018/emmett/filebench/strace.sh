8a34c11cbb7b640edbc2d8a48e054068597b51fa
Zeyuan Hu <ferrishu3886@gmail.com>
2018-10-11 16:47:36 -0500

    Update script

--- 2018/emmett/filebench/strace.sh
+++ 2018/emmett/filebench/strace.sh
@@ -8,6 +8,8 @@ FILEBENC_BIN=$HOME/filebench_install/bin/filebench
 WORKLOAD_PATH=$HOME/varmail.f
 # The directory to save the reuslt
 RES_PATH=$HOME/filebench_res
+# The directory to save filebench generated files
+FILEBENC_FILE_PATH=empty
 
 gather_data() {
     # We get the file system type
@@ -17,68 +19,72 @@ gather_data() {
     # the workload
     # Unit: KB
     kB_wrtn_before=`iostat -d /dev/sda1 | awk '$1 ~ /sda1/ {print $6}'`
-    printf "%s: %s\n", "kB_wrtn_before", "$kB_wrtn_before"
+
+    # We get the space of the device before running the workload
+    # Unit: KB
+    space_before=`du -k "$FILEBENC_FILE_PATH" | awk 'END {print $1}'`
 
     # We set the filebench output file name
     filebench_output=$RES_PATH/varmail_$fs_type
-    
+
     # We run the filebench varmail workload with strace
-    sudo strace -e trace=write -o strace.txt "$FILEBENC_BIN" -f "$WORKLOAD_PATH" | tee "$filebench_output"
-    #sudo strace -e trace=write -o strace.txt "$FILEBENC_BIN" -f "$WORKLOAD_PATH"
+    printf "==== Executing filebench: $WORKLOAD_PATH ====\n"
+    sudo strace -e trace=write -o strace.txt "$FILEBENC_BIN" -f "$WORKLOAD_PATH"
 
-    # We parse the filebench output to get the file size we work with
-    # Unit: MB (We assume the varmail worload file size is MB)
-    filesize=`awk '$2 ~ /bigfileset/ {print $18}' "$filebench_output"  | sed s'/MB//'`
-    #filesize=1601.035
-
-    # We transform the filesize into KB for the proper comparison
-    # Unit: KB
-    filesize=`echo "$filesize * 1000" | bc`
-    printf "%s: %s\n", "filesize", "$filesize"
-    
     # We use now track the `kB_wrtn` number after we run the workload
     # Unit: KB
     kB_wrtn_after=`iostat -d /dev/sda1 | awk '$1 ~ /sda1/ {print $6}'`
-    printf "%s: %s\n", "kB_wrtn_after", "$kB_wrtn_after"
+
+    # After the workload, the space difference
+    # Unit: KB
+    space_after=`du -k "$FILEBENC_FILE_PATH" | awk 'END {print $1}'`
 
     # We caclulate the difference between two kB_wrtn values
     # Unit: KB
     kB_wrtn_diff=`echo "$kB_wrtn_after - $kB_wrtn_before" | bc`
-    printf "%s: %s\n", "kB_wrtn_diff", "$kB_wrtn_diff"    
+
+    # We calculate the space difference
+    # Unit: KB
+    space_diff=`echo "$space_after - $space_before" | bc`
 
     # We parse the strace output and get the number of bytes written from
-    # write() syscall
+    # write() syscall. We exclude all the write() to fd=1
     # Unit: B
-    actual_write=`awk -F '=' '{sum += $2} END {print sum}' strace.txt`
+    actual_write=`grep '^write([2-9][0-9]*' strace.txt | awk -F '=' '{sum += $2} END {print sum}'`
 
     # Since actual_write is in bytes, we translate it into KB to have the unit
     # with the kB_wrtn_before (note we use integer division here)
     # Unit: KB
     actual_write=`echo "$actual_write / 1000" | bc`
-    printf "%s: %s\n", "actual_write", "$actual_write"    
 
     # We calculate the write amplification
-    write_amplification=`echo "$actual_write / $filesize" | bc -l`
-    printf "%s: %s\n", "write_amplification", "$write_amplification"
+    write_amplification=`echo "$kB_wrtn_diff / $actual_write" | bc -l`
 
     # We calculate the space amplfication
-    space_amplification=`echo "$kB_wrtn_diff / $filesize" | bc -l`
-    printf "%s: %s\n", "space_amplification", "$space_amplification"
+    space_amplification=`echo "$space_diff / $actual_write" | bc -l`
 
     # Now we have everything we need, let's construct the result printout string
-    # to be displayed later
-    printf "%s %s %s %s %s %s\n" "fs_type", "kB_wrtn_diff (kb)", "actual_write(kb)", "filesize(kb)", "write_amplification", "space_amplification"
-    printf "%s\n" "========================================================================================================================"
-    result_str="$fs_type \t $kB_wrtn_diff \t $actual_write \t $filesize \t $write_amplification \t $space_amplification\n"
-
-    echo "$result_str"
+    printf "==== Resulting Statistics  ====\n"
+    printf "%s : %s\n" "fs_type" "$fs_type" 
+    printf "%s: %s\n" "kB_wrtn_before (kb)" "$kB_wrtn_before"
+    printf "%s: %s\n" "kB_wrtn_after (kb)" "$kB_wrtn_after"
+    printf "%s : %s\n" "kB_wrtn_diff (kb):" "$kB_wrtn_diff"
+    printf "%s : %s\n" "actual_write (kb):" "$actual_write"
+    printf "%s : %s\n" "space_before (kb)" "$space_before" 
+    printf "%s : %s\n" "space_after (kb)" "$space_after"
+    printf "%s : %s\n" "space_diff (kb):" "$space_diff" 
+    printf "%s : %s\n" "write_amplification:" "$write_amplification" 
+    printf "%s : %s\n" "space_amplification:" "$space_amplification"
 }
 
 main(){
     # create the directory to save the results
     mkdir -p "$RES_PATH"
+    # create the directory to save the filebench-generated files
+    mkdir -p $FILEBENC_FILE_PATH
 
     gather_data
+    sudo rm -rf $FILEBENC_FILE_PATH
 }
 
 main
