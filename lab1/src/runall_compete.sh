7fc4be908f1223ae50cbf56d476e1e404bad38fd
xxks-kkk <ferrishu3886@gmail.com>
2019-02-20 13:46:21 -0600

    Modify runall_compete and add compete results

--- lab1/src/runall_compete.sh
+++ lab1/src/runall_compete.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
+gcc -O2 memory_access.c -o memory_access
+dd if=/dev/zero of=tmp bs=1 count=0 seek=2G
 
-python3 run.py "./task2 0 1 0 1 0 0 0" 5 > results/no_compete_anonymous_random_dl1.txt
-python3 run.py "./task2 0 1 0 1 0 1 0" 5 > results/no_compete_anonymous_random_dtlb.txt
-python3 run.py "./task2 0 1 0 1 0 0 1" 5 > results/compete_anonymous_random_dl1.txt
-python3 run.py "./task2 0 1 0 1 0 1 1" 5 > results/compete_anonymous_random_dtlb.txt
+python3 run.py "./memory_access 0 1 0 1 0 0" 5 > results_compete/no_compete_anonymous_random.txt
+python3 run.py "./memory_access 0 1 0 1 0 1" 5 > results_compete/compete_anonymous_random.txt
 
-python3 run.py "./task2 0 1 1 1 0 0 0" 5 > results/no_compete_private_random_dl1.txt
-python3 run.py "./task2 0 1 1 1 0 1 0" 5 > results/no_compete_private_random_dtlb.txt
-python3 run.py "./task2 0 1 1 1 0 0 1" 5 > results/compete_private_random_dl1.txt
-python3 run.py "./task2 0 1 1 1 0 1 1" 5 > results/compete_private_random_dtlb.txt
+python3 run.py "./memory_access 0 1 1 1 0 0" 5 > results_compete/no_compete_private_random.txt
+python3 run.py "./memory_access 0 1 1 1 0 1" 5 > results_compete/compete_private_random.txt
 
-python3 run.py "./task2 0 1 2 1 0 0 0" 5 > results/no_compete_shared_random_dl1.txt
-python3 run.py "./task2 0 1 2 1 0 1 0" 5 > results/no_compete_shared_random_dtlb.txt
-python3 run.py "./task2 0 1 2 1 0 0 1" 5 > results/compete_shared_random_dl1.txt
-python3 run.py "./task2 0 1 2 1 0 1 1" 5 > results/compete_shared_random_dtlb.txt
+python3 run.py "./memory_access 0 1 2 1 0 0" 5 > results_compete/no_compete_shared_random.txt
+python3 run.py "./memory_access 0 1 2 1 0 1" 5 > results_compete/compete_shared_random.txt
