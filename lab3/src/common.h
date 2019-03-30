7efdac0ec05680bfd07d425d67d1338f068960f3
xxks-kkk <ferrishu3886@gmail.com>
2019-03-30 18:22:44 -0500

    Add test_helloworld_memory_map

--- lab3/src/common.h
+++ lab3/src/common.h
@@ -1,27 +1,31 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #if defined(DEBUG) || defined(_DEBUG) || defined(PRINT_LOG)
-#define LOG(M, ...) \
-    do { \
-        fprintf(stderr, "[%s (%s:%d)]  " M "\n", __func__, __FILE__, __LINE__, ##__VA_ARGS__); \
-    } while (0)
+#define LOG(M, ...)                                                            \
+  do {                                                                         \
+    fprintf(stderr, "[%s (%s:%d)]  " M "\n", __func__, __FILE__, __LINE__,     \
+            ##__VA_ARGS__);                                                    \
+  } while (0)
 #else
-#define LOG(M, ...) do {} while (0)
+#define LOG(M, ...)                                                            \
+  do {                                                                         \
+  } while (0)
 #endif
 
-#define CHECK(EXPR, M, ...) \
-do { \
-    if (!(EXPR)) { \
-        fprintf(stderr, "[%s (%s:%d) ERROR]  " M "\n", __func__, __FILE__, __LINE__, ##__VA_ARGS__); \
-        exit(EXIT_FAILURE); \
-    } \
-} while (0)
+#define CHECK(EXPR, M, ...)                                                    \
+  do {                                                                         \
+    if (!(EXPR)) {                                                             \
+      fprintf(stderr, "[%s (%s:%d) ERROR]  " M "\n", __func__, __FILE__,       \
+              __LINE__, ##__VA_ARGS__);                                        \
+      exit(EXIT_FAILURE);                                                      \
+    }                                                                          \
+  } while (0)
 
-#define LOADER_START_ADDRESS  0x7f400000
-#define DEFAULT_STACK_SIZE    81920
+#define LOADER_START_ADDRESS 0x7f400000
+#define DEFAULT_STACK_SIZE 8388608
 
-#endif  // COMMON_H
+#endif // COMMON_H
