aae4e180fa426b3e983a7de2e25a815c584dec80
xxks-kkk <ferrishu3886@gmail.com>
2020-11-15 16:26:00 -0600

    Add hw4 solution

--- 2020/coursera-programming-languages/partB/week4/hw4/hw4.rkt
+++ 2020/coursera-programming-languages/partB/week4/hw4/hw4.rkt
@@ -5,3 +5,66 @@
 
 ;; put your code below
 
+(define (sequence low high stride)
+  (cond [(> low high) null]
+        [#t (cons low (sequence (+ low stride) high stride))]))
+
+(define (string-append-map xs suffix)
+  (map (lambda (str) (string-append str suffix)) xs))
+
+(define (list-nth-mod xs n)
+  (cond [(< n 0) (error "list-nth-mod: negative number")]
+        [(null? xs) (error "list-nth-mod: empty list")]
+        [#t (car (list-tail xs (remainder n (length xs))))]))
+
+(define (stream-for-n-steps s n)
+  (cond [(= n 0) null]
+        [#t (let ([res (s)])
+              (cons (car res) (stream-for-n-steps (cdr res) (- n 1))))]))
+
+; Question: why the body is (f 1) instead of (lambda () (f 1))?
+(define (funny-number-stream)
+  (define (f x)
+      (cons
+       (if (= (remainder x 5) 0)
+           (* -1 x)
+           x)
+       (lambda () (f (+ x 1)))))
+  (f 1))
+
+(define dan-then-dog  
+   (letrec ([f (lambda (x) (cons x (lambda () (f (if (equal? x "dog.jpg") "dan.jpg" "dog.jpg")))))])
+    (lambda () (f "dan.jpg"))))
+
+(define (stream-add-zero s)
+  (define tmp (s))
+  (lambda () (cons (cons 0 (car tmp)) (stream-add-zero (cdr tmp)))))
+
+(define (cycle-lists xs ys)
+  (define (f n) (cons (cons (list-nth-mod xs n) (list-nth-mod ys n)) (lambda () (f (+ n 1)))))
+  (lambda () (f 0)))
+
+(define (vector-assoc v vec)
+  (define (f n) (if (>= n (vector-length vec))
+                    #f
+                    (let ([current (vector-ref vec n)]
+                          [next (lambda (x) (f (+ x 1)))])
+                      (if (pair? current)
+                          (if (equal? (car current) v) current (next n))
+                          (next n)))))
+  (f 0))
+
+(define (cached-assoc xs n)
+  (letrec ([memo (make-vector n #f)]
+           [pos 0]
+           [f (lambda (v)
+                (let ([ans (vector-assoc v memo)])
+                (if ans
+                    ans
+                    (let ([new-ans (assoc v xs)])
+                      (if new-ans
+                          (begin (vector-set! memo pos new-ans)
+                                 (set! pos (remainder (add1 pos) n))
+                                 new-ans)
+                          #f)))))])
+    f))
--- 2020/coursera-programming-languages/partB/week4/hw4/hw4test.rkt
+++ 2020/coursera-programming-languages/partB/week4/hw4/hw4test.rkt
@@ -5,40 +5,49 @@
 
 ;; Be sure to put your homework file in the same folder as this test file.
 ;; Uncomment the line below and change HOMEWORK_FILE to the name of your homework file.
-;;(require "HOMEWORK_FILE")
+(require "hw4.rkt")
 
 (require rackunit)
 
 ;; Helper functions
 (define ones (lambda () (cons 1 ones)))
 (define a 2)
+(define (f x) (cons x (lambda () (f (+ x 1)))))
+(define nats (lambda () (f 1)))
 
 (define tests
   (test-suite
    "Sample tests for Assignment 4"
    
    ; sequence test
-   (check-equal? (sequence 0 5 1) (list 0 1 2 3 4 5) "Sequence test")
+   (check-equal? (sequence 0 5 1) (list 0 1 2 3 4 5) "sequence test 1")
+   (check-equal? (sequence 3 11 2) (list 3 5 7 9 11) "sequence test 2")
+   (check-equal? (sequence 3 8 3) (list 3 6) "sequence test 3")
+   (check-equal? (sequence 3 2 1) null "sequence test 4")
 
    ; string-append-map test
    (check-equal? (string-append-map 
                   (list "dan" "dog" "curry" "dog2") 
                   ".jpg") '("dan.jpg" "dog.jpg" "curry.jpg" "dog2.jpg") "string-append-map test")
-   
+
    ; list-nth-mod test
    (check-equal? (list-nth-mod (list 0 1 2 3 4) 2) 2 "list-nth-mod test")
    
    ; stream-for-n-steps test
    (check-equal? (stream-for-n-steps ones 2) (list 1 1) "stream-for-n-steps test")
+   (check-equal? (stream-for-n-steps nats 4) (list 1 2 3 4) "stream-for-n-steps test2")
    
    ; funny-number-stream test
    (check-equal? (stream-for-n-steps funny-number-stream 16) (list 1 2 3 4 -5 6 7 8 9 -10 11 12 13 14 -15 16) "funny-number-stream test")
    
    ; dan-then-dog test
    (check-equal? (stream-for-n-steps dan-then-dog 1) (list "dan.jpg") "dan-then-dog test")
+   (check-equal? (stream-for-n-steps dan-then-dog 2) (list "dan.jpg" "dog.jpg") "dan-then-dog test2")
+   (check-equal? (stream-for-n-steps dan-then-dog 3) (list "dan.jpg" "dog.jpg" "dan.jpg") "dan-then-dog test3")
    
    ; stream-add-zero test
    (check-equal? (stream-for-n-steps (stream-add-zero ones) 1) (list (cons 0 1)) "stream-add-zero test")
+   (check-equal? (stream-for-n-steps (stream-add-zero nats) 3) (list (cons 0 1) (cons 0 2) (cons 0 3)) "stream-add-zero test2")
    
    ; cycle-lists test
    (check-equal? (stream-for-n-steps (cycle-lists (list 1 2 3) (list "a" "b")) 3) (list (cons 1 "a") (cons 2 "b") (cons 3 "a")) 
